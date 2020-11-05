0a761b451cb29b40aa55d38a677480d08170de15
Teikn Smith <spencerteiknsmith@gmail.com>
2020-11-04 17:55:00 -0700

    Re-style for readbility in VS Code primarily with converting indents to spaces

--- GradeInstance.h
+++ GradeInstance.h
@@ -1,9 +1,10 @@
 #ifndef GRADE_INSTANCE_H_
 #define GRADE_INSTANCE_H_
 
-#include "Instance.h"
-#include <string>
 #include <sstream>
+#include <string>
+
+#include "Instance.h"
 
 class GradeInstance : public Instance {
  private:
@@ -11,10 +12,9 @@ class GradeInstance : public Instance {
   string before;
   string after;
   string current;
+
  public:
-  GradeInstance(string row) {
-    parse(row);
-  }
+  GradeInstance(string row) { parse(row); }
   void parse(string row) {
     string cell;
     string assignment;
@@ -27,7 +27,7 @@ class GradeInstance : public Instance {
     date += cell;
     addDate(date);
 
-    getline(line, cell, ',');//anonymous
+    getline(line, cell, ',');  // anonymous
     getline(line, cell, ',');
     addStudent(cell);
     getline(line, cell, ',');
@@ -41,21 +41,11 @@ class GradeInstance : public Instance {
     getline(line, cell, ',');
     addCurrent(cell);
   }
-  void addAssignment(string assignment) {
-    this->assignment = assignment;
-  }
-  string getAssignment() const {
-    return assignment;
-  }
-  void addBefore(string before) {
-    this->before = before;
-  }
-  void addAfter(string after) {
-    this->after = after;
-  }
-  void addCurrent(string current) {
-    this->current = current;
-  }
+  void addAssignment(string assignment) { this->assignment = assignment; }
+  string getAssignment() const { return assignment; }
+  void addBefore(string before) { this->before = before; }
+  void addAfter(string after) { this->after = after; }
+  void addCurrent(string current) { this->current = current; }
 };
 
 #endif
--- GradingStatsWorker.h
+++ GradingStatsWorker.h
@@ -1,10 +1,9 @@
-#include <string>
-#include <sstream>
-#include <vector>
 #include <fstream>
-#include <map>
-
 #include <iostream>
+#include <map>
+#include <sstream>
+#include <string>
+#include <vector>
 
 class GradingStatsWorker {
  private:
@@ -26,9 +25,7 @@ class GradingStatsWorker {
       std::getline(rowSS, taNetid, ',');
       std::getline(rowSS, lockType, ',');
     }
-    bool isLock() {
-      return lockType == "LOCK";
-    }
+    bool isLock() { return lockType == "LOCK"; }
   };
 
   std::vector<Event> events;
@@ -37,13 +34,12 @@ class GradingStatsWorker {
     std::string studentName;
     std::string labName;
     StudentAssignment(const std::string &s, const std::string &l)
-     : studentName(s), labName(l) {}
-    bool operator< (const StudentAssignment o) const {
+        : studentName(s), labName(l) {}
+    bool operator<(const StudentAssignment o) const {
       if (this->studentName == o.studentName) {
-	return this->labName < o.labName;
-      }
-      else {
-	return this->studentName < o.studentName;
+        return this->labName < o.labName;
+      } else {
+        return this->studentName < o.studentName;
       }
     }
   };
@@ -51,9 +47,11 @@ class GradingStatsWorker {
   std::map<StudentAssignment, std::vector<Event>> mappedEvents;
   std::map<std::string, int> taCounts;
 
-  unsigned long long int secondsInYear (const std::string &timeStamp) {
-    unsigned long long int yr = 0, mon = 0, day = 0, hr = 0, min = 0, sec = 0, ms = 0;
-    sscanf(timeStamp.c_str(), "%4llu-%2llu-%2lluT%2llu:%2llu:%2llu.%6llu", &yr, &mon, &day, &hr, &min, &sec, &ms);
+  unsigned long long int secondsInYear(const std::string &timeStamp) {
+    unsigned long long int yr = 0, mon = 0, day = 0, hr = 0, min = 0, sec = 0,
+                           ms = 0;
+    sscanf(timeStamp.c_str(), "%4llu-%2llu-%2lluT%2llu:%2llu:%2llu.%6llu", &yr,
+           &mon, &day, &hr, &min, &sec, &ms);
     mon *= 31;
     day += mon;
     day *= 24;
@@ -65,27 +63,28 @@ class GradingStatsWorker {
     return sec;
   }
 
-  std::string whoGradedThis (const std::vector<Event> &events) {
+  std::string whoGradedThis(const std::vector<Event> &events) {
     std::vector<std::pair<std::string, unsigned long long int>> taSeconds;
-    std::map <std::string, unsigned long long int> lockTimes;
+    std::map<std::string, unsigned long long int> lockTimes;
     for (Event e : events) {
       if (e.isLock()) {
-	lockTimes[e.taNetid] = secondsInYear(e.timeStamp);
-      }
-      else {
-	if (lockTimes.count(e.taNetid) == 0 || lockTimes[e.taNetid] == 0) {
-	  //do nothing
-	}
-	else {
-	  taSeconds.push_back(std::make_pair(e.taNetid, secondsInYear(e.timeStamp) - lockTimes[e.taNetid]));
-	}
+        lockTimes[e.taNetid] = secondsInYear(e.timeStamp);
+      } else {
+        if (lockTimes.count(e.taNetid) == 0 || lockTimes[e.taNetid] == 0) {
+          // do nothing
+        } else {
+          taSeconds.push_back(std::make_pair(
+              e.taNetid, secondsInYear(e.timeStamp) - lockTimes[e.taNetid]));
+        }
       }
     }
 
-    //taSeconds now holds the amount of time the assignment was unlocked by each TA in order
+    // taSeconds now holds the amount of time the assignment was unlocked by
+    // each TA in order
 
     const int MIN_SECONDS_THRESHOLD = 15;
-    std::vector<std::pair<std::string, unsigned long long int>> longEnoughTaSeconds;
+    std::vector<std::pair<std::string, unsigned long long int>>
+        longEnoughTaSeconds;
     for (std::pair<std::string, unsigned long long int> count : taSeconds) {
       if (count.second > MIN_SECONDS_THRESHOLD) {
         longEnoughTaSeconds.push_back(count);
@@ -94,8 +93,7 @@ class GradingStatsWorker {
 
     if (longEnoughTaSeconds.empty()) {
       return "";
-    }
-    else {
+    } else {
       return longEnoughTaSeconds.front().first;
     }
   }
@@ -108,10 +106,11 @@ class GradingStatsWorker {
     for (it = mappedEvents.cbegin(); it != mappedEvents.cend(); ++it) {
       std::string taName = whoGradedThis(it->second);
       if (!taName.empty()) {
-	taCounts[taName]++;
+        taCounts[taName]++;
       }
     }
   }
+
  public:
   void readInStats(const std::string &fileName) {
     std::ifstream in(fileName);
@@ -127,13 +126,10 @@ class GradingStatsWorker {
   }
   void writeOutStats(const std::string &fileName) {
     std::ofstream out(fileName);
-    for (std::map<std::string, int>::const_iterator it
-	   = taCounts.cbegin();
-	 it != taCounts.cend();
-	 ++it) {
+    for (std::map<std::string, int>::const_iterator it = taCounts.cbegin();
+         it != taCounts.cend(); ++it) {
       out << it->first << ',' << it->second << '\n';
     }
     out.close();
   }
-
-  };
+};
